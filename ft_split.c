ede3520de6aec2e79e4f9b613d8a46d83d0f486a
Alexandre <alexandre.tomasi80@gmail.com>
2024-10-03 18:45:20 +0200

    ft_split fonctionnel mais pas opti

--- ft_split.c
+++ ft_split.c
@@ -3,26 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atomasi <atomasi@student.42.fr>            +#+  +:+       +#+        */
+/*   By: alexandre <alexandre@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 15:34:06 by atomasi           #+#    #+#             */
-/*   Updated: 2024/10/03 16:31:41 by atomasi          ###   ########.fr       */
+/*   Updated: 2024/10/03 18:44:44 by alexandre        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
  #include <stdlib.h>
 
-
-int	counter(char const *s)
+int malloc_words(char **res,const char *s, char c)
 {
+	int word;
+	int countl;
 	int i;
 
+	word = 0;
 	i = 0;
 	while(s[i])
 	{
-		i++;
+		countl = 0;
+		while (s[i] == c)
+			i++;
+		while (s[i] != c && s[i])
+		{
+			countl++;
+			i++;
+		}
+		res[word] = malloc(sizeof(char) * countl + 1);
+		if (res[word] == NULL)
+			return (0);
+		word++;
+
 	}
-	return (i);
+	return (1);
 }
 
 int counterc(char const *s, char c)
@@ -38,7 +52,7 @@ int counterc(char const *s, char c)
 			count++;
 		i++;
 	}
-	return (count + i);
+	return (count + 1);
 }
 
  char	**ft_split(char const *s, char c)
@@ -52,12 +66,18 @@ int counterc(char const *s, char c)
 	i = 0;
 	jres = 0;
 	ires = 0;
-	if (res == NULL)
+	if (res == NULL || !malloc_words(res, s, c))
 		return (0);
+	while (s[i] == c)
+		i++;
 	while(s[i])
 	{
 		if (s[i] == c)
+		{
 			ires++;
+			jres = 0;
+			i++;
+		}
 		res[ires][jres] = s[i];
 		jres++;
 		i++;
@@ -65,16 +85,18 @@ int counterc(char const *s, char c)
 	return (res);
  }
 
+
 #include <stdio.h>
 
 int main()
 {
-	char *text = "je teste, ma,fonction";
+	char *text = ",,je,teste,ma,fonction";
 	char sep = ',';
 	char **res = ft_split(text, sep);
 
-	printf("Premiere phrase : %s", res[0]);
-	printf("Deuxieme phrase : %s", res[1]);
-	printf("Troisieme phrase : %s", res[2]);
+	printf("Premiere phrase : %s\n", res[0]);
+	printf("Deuxieme phrase : %s\n", res[1]);
+	printf("Troisieme phrase : %s\n", res[2]);
+	printf("Quatrieme phrase : %s\n", res[3]);
 
 }
