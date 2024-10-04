b313fdbede22bb0b8cb3ea77328b2884c52c58e9
Alexandre Tomasi <atomasi@c2r5s4.42lausanne.ch>
2024-10-04 12:14:03 +0200

    Fini ft_split et itoa et commencement de strmapi

---
+++ ft_itoa.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atomasi <atomasi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/04 10:50:33 by atomasi           #+#    #+#             */
+/*   Updated: 2024/10/04 12:12:10 by atomasi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	len(int n)
+{
+	int	nb;
+	int	res;
+
+	nb = n;
+	res = 0;
+	if (nb < 0)
+	{
+		res++;
+		nb = nb * -1;
+	}
+	while (nb > 0)
+	{
+		nb = nb / 10;
+		res++;
+	}
+	return (res);
+}
+
+char	*ft_itoa(int n)
+{
+	int		i	;
+	int		malus;
+	char	*res;
+
+	res = malloc(sizeof(char) * len(n));
+	i = len(n);
+	malus = 0;
+	if (n == 0)
+		return ("0");
+	if (n == -2147483648)
+		return ("-2147483648");
+	if (n < 0)
+	{
+		n *= -1;
+		malus = 1;
+	}
+	res[i] = '\0';
+	while (--i >= 0)
+	{
+		res[i] = (n % 10) + 48;
+		n = n / 10;
+	}
+	if (malus)
+		res[0] = '-';
+	return (res);
+}
+/*
+#include <stdio.h>
+
+int main()
+{
+	printf("Test de la fonction len : %s", ft_itoa(0));
+}
+ */
--- ft_split.c
+++ ft_split.c
@@ -3,24 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alexandre <alexandre@student.42.fr>        +#+  +:+       +#+        */
+/*   By: atomasi <atomasi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 15:34:06 by atomasi           #+#    #+#             */
-/*   Updated: 2024/10/03 18:44:44 by alexandre        ###   ########.fr       */
+/*   Updated: 2024/10/04 11:06:59 by atomasi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
- #include <stdlib.h>
+#include <stdlib.h>
 
-int malloc_words(char **res,const char *s, char c)
+void	fill(char **res, const char *s, char c)
 {
-	int word;
-	int countl;
-	int i;
+	int	i;
+	int	jres;
+	int	ires;
+
+	i = 0;
+	jres = 0;
+	ires = 0;
+	while (s[i] == c)
+		i++;
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			ires++;
+			jres = 0;
+		}
+		while (s[i] == c)
+			i++;
+		res[ires][jres] = s[i];
+		jres++;
+		i++;
+	}
+	res[ires] = NULL;
+}
+
+int	malloc_words(char **res, const char *s, char c)
+{
+	int	word;
+	int	countl;
+	int	i;
 
 	word = 0;
 	i = 0;
-	while(s[i])
+	while (s[i])
 	{
 		countl = 0;
 		while (s[i] == c)
@@ -34,19 +61,18 @@ int malloc_words(char **res,const char *s, char c)
 		if (res[word] == NULL)
 			return (0);
 		word++;
-
 	}
 	return (1);
 }
 
-int counterc(char const *s, char c)
+int	counterc(char const *s, char c)
 {
-	int i;
-	int count;
+	int	i;
+	int	count;
 
 	i = 0;
 	count = 0;
-	while(s[i])
+	while (s[i])
 	{
 		if (s[i] == c)
 			count++;
@@ -55,42 +81,23 @@ int counterc(char const *s, char c)
 	return (count + 1);
 }
 
- char	**ft_split(char const *s, char c)
- {
-	 char **res;
-	 int i;
-	 int jres;
-	 int ires;
+char	**ft_split(char const *s, char c)
+{
+	char	**res;
 
-	res = malloc(sizeof(char*) * counterc(s, c));
-	i = 0;
-	jres = 0;
-	ires = 0;
+	res = malloc(sizeof(char *) * counterc(s, c) + 1);
 	if (res == NULL || !malloc_words(res, s, c))
 		return (0);
-	while (s[i] == c)
-		i++;
-	while(s[i])
-	{
-		if (s[i] == c)
-		{
-			ires++;
-			jres = 0;
-			i++;
-		}
-		res[ires][jres] = s[i];
-		jres++;
-		i++;
-	}
+	fill(res, s, c);
 	return (res);
- }
+}
 
-
+/*
 #include <stdio.h>
 
 int main()
 {
-	char *text = ",,je,teste,ma,fonction";
+	char *text = ",,jeteste,,afo,ncti,on,";
 	char sep = ',';
 	char **res = ft_split(text, sep);
 
@@ -98,5 +105,7 @@ int main()
 	printf("Deuxieme phrase : %s\n", res[1]);
 	printf("Troisieme phrase : %s\n", res[2]);
 	printf("Quatrieme phrase : %s\n", res[3]);
+	printf("Derniere phrase : %s\n", res[4]);
 
 }
+ */
